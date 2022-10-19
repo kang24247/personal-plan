1f052a62cc97342624f80252ea5481d243b64413
kangyu <kangyu24247@163.com>
2022-10-19 23:18:10 +0800

    set-login-background-image

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,10 +1,19 @@
-import React from 'react';
-
+import React,{ useEffect } from 'react';
+import '@/assets/cssCollection/login.less'
 function Login() {
+  // useEffect(()=>{
+  //   const appWrapper:any = document.querySelector('.login-wrapper')
+  //   setInterval(()=>{
+  //     if(appWrapper) {
+  //       let imagePath = `@/assets/background${Math.floor(Math.random()*5+1)}.jpg`
+  //       appWrapper.style.backgroundImage = `url(${imagePath})`
+  //     }
+  //   },10000)
+  // },[])
   return (
-    <div className="App">
-      <header className="App-header">
-       这是登录页面的头部内容
+    <div className="login-wrapper">
+      <header className="login-header">
+        这是登录页面的头部内容
       </header>
       <section>
         这是登录页的主要内容区域
